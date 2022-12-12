97c308d7d3bfae46e7189169c16493b9780dc534
Aleksandra-Stachniak <81320689+Aleksandra-Stachniak@users.noreply.github.com>
2022-12-12 22:42:51 +0100

    Update and rename BO2.py to data_structures.py

--- data_structures.py
+++ data_structures.py
@@ -33,51 +33,6 @@ def parameters(number_products:int):
     total_weight = np.random.randint(7000, 15000)
     return product_weights, time_1, time_2, time_3, profits, total_time_1, total_time_2, total_time_3, total_weight
 
-def function(sollution:np.array, profits:np.array):
-    """
-      Funkcja obliczająca wartość funkcji celu. 
-      Naszą funkcją celu jest sumaryczny iloczyn wielkości produkcji i zysku ze sprzedaży dla poszczególnego produktu.
-`    """
-    
-    sollution_T = sollution.transpose()
-    fun = profits.dot(sollution_T)
-    return fun
-
-def production_volume(number_products:int, product_weights:np.array, time_1:np.array, time_2:np.array, time_3:np.array,
-                      profits:np.array, total_time_1:int, total_time_2:int, total_time_3:int, total_weight:int):
-    """
-      Funkcja uwzględnia ograniczenia dla zadanego rozwiązania i sprawdza jego dopuszczalność
-      
-      Zwracamy rozwiązanie, którym jest wektor zawierający wielkość produkcji dla poszczególnego produktu 
-`    """
-    
-    products = np.random.randint(1, 100, size=(1,number_products))
-    products_T = products.transpose()
-
-    while True:
-      products = np.random.randint(1, 100, size=(1,number_products))
-      products_T = products.transpose()
-      if time_1.dot(products_T) <= total_time_1:
-        break
-
-    while True:
-      products = np.random.randint(1, 100, size=(1,number_products))
-      products_T = products.transpose()
-      if time_2.dot(products_T) <= total_time_2:
-        break
-
-    while True:
-      products = np.random.randint(1, 100, size=(1,number_products))
-      products_T = products.transpose()
-      if time_3.dot(products_T) <= total_time_3:
-        break
-
-    while True:
-      products = np.random.randint(1, 100, size=(1,number_products))
-      products_T = products.transpose()
-      if product_weights.dot(products_T) <= total_weight:
-        break
-    return products
 
 def main():
     number_products = 10
