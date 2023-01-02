2ccf9ae7387158b7a5cf8b9a2462c2c319253787
PiotrSztos <piotrstosik.44@gmail.com>
2023-01-02 22:44:41 +0100

    postępy

--- gui_pso_v1.py
+++ gui_pso_v1.py
@@ -52,7 +52,25 @@ l_r2.pack()
 p_r2.pack()
 ls1a = tkinter.Label(okno1, text='')
 ls1a.pack()
+def oblicz(p_n,p_N_max,p_omega,p_c1,p_c2,p_r1,p_r2,e_s,e_g):
+    def f():
+        n = p_n.get()
+        N_max = p_N_max.get()
+        w = p_omega.get()
+        c1 = p_c1.get()
+        c2 = p_c2.get()
+        r1 = p_r1.get()
+        r2 = p_r2.get()
 
+        n_ = int(n)
+        w_ = int(w)
+        c1_ = int(c1)
+        c2_ = int(c2)
+        ps = str(n_ + w_)
+        gs = str(c1_ + c2_)
+        e_s['text'] = ps
+        e_g['text'] = gs
+    return f
 #funkcja czyszcząca parametry
 def button_func(a, b, c, d, e, f, g):
     def func():
@@ -69,24 +87,15 @@ b = tkinter.Button(okno1, text='clear', command=button_func(p_n, p_N_max, p_omeg
 b.pack()
 
 #Pobieranie parametrów
-def get_params(p1, p2, p3, p4, p5, p6, p7):
+#def get_params(p1, p2, p3, p4, p5, p6, p7):
     #if int(p_n.get()) > 0 and int(p_N_max.get()) > 0 and 0 < int(p_omega.get()) < 1 and 0 < int(p_c1.get()) < 1 and 0 < int(p_c2.get()) < 1 and 0 < int(p_r1.get()) < 1 and 0 < int(p_r2.get()) < 1:
-    n = p1.get()
-    N_max = p2.get()
-    w = p3.get()
-    c1 = p4.get()
-    c2 = p5.get()
-    r1 = p6.get()
-    r2 = p7.get()
-    return n, N_max, w, c1, c2, r1, r2
+
+
     #else:
         #p_error['text'] = 'Błąd - popraw dane'
 
-n, N_max, w, c1, c2, r1, r2 = get_params(p_n, p_N_max, p_omega, p_c1, p_c2, p_r1, p_r2)
+#n, N_max, w, c1, c2, r1, r2 = get_params(p_n, p_N_max, p_omega, p_c1, p_c2, p_r1, p_r2)
 
-#test
-ps = n + c1
-gs = r1 + r2
 
 #ps = particle_swarm.start_solution
 #gs = particle_swarm.global_solution
@@ -97,22 +106,25 @@ gs = r1 + r2
 ls2 = tkinter.Label(okno1, text='')
 ls2.pack()
 l_start_solution = tkinter.Label(okno1, text='Rozwiązanie początkowe:')
-e_start_solution = tkinter.Label(okno1, text=ps, bg="#C0CBCB", width=55, anchor="w", borderwidth=2)
-l_start_solution.pack()
-e_start_solution.pack()
+e_start_solution = tkinter.Label(okno1, bg="#C0CBCB", width=55, anchor="w", borderwidth=2)
+
 l_global_solution = tkinter.Label(okno1, text='Rozwiązanie optymalne:')
-e_global_solution = tkinter.Label(okno1, text=gs, bg="#C0CBCB", width=55, anchor="w", borderwidth=2)
-l_global_solution.pack()
-e_global_solution.pack()
+e_global_solution = tkinter.Label(okno1, bg="#C0CBCB", width=55, anchor="w", borderwidth=2)
+
 l_fun_celu = tkinter.Label(okno1, text='Wartość funkcji celu:')
 e_fun_celu = tkinter.Label(okno1, bg="#C0CBCB", width=55, anchor="w", borderwidth=2)
-l_fun_celu.pack()
-e_fun_celu.pack()
 l_iter = tkinter.Label(okno1, text='Ilosć iteracji:')
 e_iter = tkinter.Label(okno1, bg="#C0CBCB", width=55, anchor="w", borderwidth=2)
+button_gen = tkinter.Button(okno1, text='wynik', command=oblicz(p_n,p_N_max,p_omega,p_c1,p_c2,p_r1,p_r2,e_start_solution,e_global_solution))
+button_gen.pack()
+l_start_solution.pack()
+e_start_solution.pack()
+l_global_solution.pack()
+e_global_solution.pack()
+l_fun_celu.pack()
+e_fun_celu.pack()
 l_iter.pack()
 e_iter.pack()
-
 ls4 = tkinter.Label(okno1, text='')
 ls4.pack()
 p_error = tkinter.Entry(okno1)
